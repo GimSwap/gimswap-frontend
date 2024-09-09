8fc63a45df0e5000b95e14b0a7a506a5aa977c69
GimSwap <admin@gimswap.com>
2024-09-09 18:05:43 +0900

    UPDATE : network change

--- src/app/[locales]/swap/_components/SwapButton.tsx
+++ src/app/[locales]/swap/_components/SwapButton.tsx
@@ -2,7 +2,11 @@ import Button from '@/src/components/Button';
 import SwapConfirmPopup from '@/src/components/popups/SwapConfirmPoup';
 import { usePopupStore } from '@/src/lib/stores/popupStore/PopupStoreProvider';
 import { TokenType } from '@/src/lib/types/TokenType';
-import { useWeb3Modal, useWeb3ModalAccount } from '@web3modal/ethers/react';
+import {
+  useWeb3Modal,
+  useWeb3ModalAccount,
+  useSwitchNetwork,
+} from '@web3modal/ethers/react';
 
 interface SwapButtonProps {
   tokens: { pay: TokenType; receive: TokenType };
@@ -12,20 +16,28 @@ interface SwapButtonProps {
 }
 
 export const SwapButton = ({
-  tokens,
-  amount,
-  fee,
-  isEnoughBalance,
+ tokens,
+ amount,
+ fee,
+ isEnoughBalance,
 }: SwapButtonProps) => {
   const { openPopup } = usePopupStore((state) => state);
   const { isConnected } = useWeb3ModalAccount();
   const { open } = useWeb3Modal();
+  const { chainId } = useWeb3ModalAccount();
+  const { switchNetwork } = useSwitchNetwork();
 
   const handleButtonClick = () => {
     if (!isConnected) {
       open();
       return;
     }
+
+    if (chainId !== Number(process.env.NEXT_PUBLIC_KLAYTN_CHAIN_ID)) {
+      switchNetwork(Number(process.env.NEXT_PUBLIC_KLAYTN_CHAIN_ID));
+      return;
+    }
+
     openPopup(SwapConfirmPopup, {
       tokens,
       amount,
@@ -35,10 +47,13 @@ export const SwapButton = ({
 
   const buttonTitle = () => {
     if (!isConnected) return 'Connect Wallet';
+    if (chainId !== Number(process.env.NEXT_PUBLIC_KLAYTN_CHAIN_ID))
+      return 'Switch the Network';
     if (!amount) return 'Enter an amount';
     if (!isEnoughBalance) return 'Insufficient Balance';
     else return 'Swap';
   };
+
   return (
     <Button
       onClick={handleButtonClick}

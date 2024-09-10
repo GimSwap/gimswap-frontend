9a9e929c6fd0e00646e0cbebf31a5d9e772eb4cf
GimSwap <admin@gimswap.com>
2024-09-10 04:47:13 +0000

    Apply changes from main repo

--- src/components/WalletConnectButton.tsx
+++ src/components/WalletConnectButton.tsx
@@ -2,12 +2,12 @@ import {
   useWeb3Modal,
   useWeb3ModalAccount,
   useWalletInfo,
-} from "@web3modal/ethers/react";
-import { shortenAddress } from "../lib/utils/shortenAddress";
-import { WALLET_ICONS } from "../lib/constants/walletIcons";
+} from '@web3modal/ethers/react';
+import { shortenAddress } from '../lib/utils/shortenAddress';
+import { WALLET_ICONS } from '../lib/constants/walletIcons';
 
 interface WalletConnectButtonProps {
-  size: "small" | "large";
+  size: 'small' | 'large';
 }
 
 export default function WalletConnectButton({
@@ -20,7 +20,7 @@ export default function WalletConnectButton({
     try {
       open();
     } catch (err) {
-      console.error("Failed to open wallet connect modal", err);
+      console.error('Failed to open wallet connect modal', err);
       close();
     }
   };
@@ -29,27 +29,25 @@ export default function WalletConnectButton({
   return (
     <button
       className={`bg-purple-500 font-medium text-black-1 rounded-lg ${
-        size === "small" ? "c1 py-2 px-3" : "py-3 px-4 text-h5 font-bold"
+        size === 'small' ? 'c1 py-2 px-3' : 'py-3 px-4 text-h5 font-bold'
       }`}
       onClick={handleOpen}
     >
       {address ? (
         <section className="flex gap-1 justify-center">
-          {walletInfo?.icon && (
-            <section className="flex gap-1">
-              {WalletIcon && (
-                <WalletIcon
-                  className={`${
-                    size === "small" ? "w-4 h-4 p-[1.6px]" : "w-6 h-6 p-[2.4px]"
-                  }" bg-black-1 rounded-full`}
-                />
-              )}
-              {shortenAddress(address)}
-            </section>
-          )}
+          <section className="flex gap-1">
+            {WalletIcon && (
+              <WalletIcon
+                className={`${
+                  size === 'small' ? 'w-4 h-4 p-[1.6px]' : 'w-6 h-6 p-[2.4px]'
+                }" bg-black-1 rounded-full`}
+              />
+            )}
+            {shortenAddress(address)}
+          </section>
         </section>
       ) : (
-        "Connect"
+        'Connect'
       )}
     </button>
   );
--- src/lib/constants/walletIcons.tsx
+++ src/lib/constants/walletIcons.tsx
@@ -1,5 +1,6 @@
-import MetamaskIcon from "@/public/svg/wallet/metamask.svg";
+import MetamaskIcon from '@/public/svg/wallet/metamask.svg';
 
 export const WALLET_ICONS: Record<string, React.ElementType> = {
   MetaMask: MetamaskIcon,
+  'MetaMask Wallet': MetamaskIcon,
 };

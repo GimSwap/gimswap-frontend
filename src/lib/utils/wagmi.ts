e4b4ed62c4875aabb28cb196af782853157fc518
GimSwap <admin@gimswap.com>
2024-10-16 06:27:13 +0000

    Apply changes from main repo

--- src/lib/utils/wagmi.ts
+++ src/lib/utils/wagmi.ts
@@ -2,6 +2,7 @@ import { http, createConfig, createStorage, noopStorage } from 'wagmi';
 import { kaia } from 'wagmi/chains';
 import { injected, walletConnect } from 'wagmi/connectors';
 import { kaikasConnector } from '@/src/lib/utils/wallets/kaiaWallet';
+import { fallback } from 'viem';
 
 const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 if (!projectId) throw new Error('there is no projectId');
@@ -14,7 +15,13 @@ export const wagmiConfig = createConfig({
     injected({ target: 'metaMask', shimDisconnect: true }),
   ],
   transports: {
-    [kaia.id]: http(),
+    [kaia.id]: fallback([
+      http('https://public-en.node.kaia.io'),
+      http('https://kaia-mainnet.rpc.grove.city/v1/803ceedf'),
+      http('https://klaytn.drpc.org'),
+      http('https://go.getblock.io/d7094dbd80ab474ba7042603fe912332'),
+      http('https://1rpc.io/klay'),
+    ]),
   },
   ssr: true,
   storage: createStorage({

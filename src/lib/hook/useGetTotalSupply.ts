7cb19a8201cef148376fc22a90abacf2385a56e8
GimSwap <admin@gimswap.com>
2024-10-16 05:51:06 +0000

    Apply changes from main repo

--- src/lib/hook/useGetTotalSupply.ts
+++ src/lib/hook/useGetTotalSupply.ts
@@ -1,16 +1,22 @@
 import { useEffect, useState } from 'react';
 import totalSupplyAbi from '@/src/lib/utils/abis/getTotalSupplyAbi.json';
 import { CONTRACT_ADDRESS } from '@/src/lib/constants/contractAddress';
-import { createPublicClient, formatUnits } from 'viem';
+import { createPublicClient, fallback, formatUnits, http } from 'viem';
 import { KRWO } from '@/src/lib/constants/token';
 import { kaia } from 'wagmi/chains';
-import { http } from 'wagmi';
 
 export const useGetTotalSupply = () => {
   const [totalSupply, setTotalSupply] = useState<string | null>(null);
   const client = createPublicClient({
     chain: kaia,
-    transport: http(kaia.rpcUrls.default.http[0]),
+    transport: fallback([
+      http('https://public-en.node.kaia.io'),
+      http('https://kaia-mainnet.rpc.grove.city/v1/803ceedf'),
+      http('https://klaytn.drpc.org'),
+      http('https://go.getblock.io/d7094dbd80ab474ba7042603fe912332'),
+      http('https://1rpc.io/klay'),
+    ]),
+
     cacheTime: 0,
   });
   useEffect(() => {

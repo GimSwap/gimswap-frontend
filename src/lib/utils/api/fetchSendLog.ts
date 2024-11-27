1cefa5539ac506a8e4acacf192665a81a1ddea62
GimSwap <admin@gimswap.com>
2024-11-27 07:01:21 +0000

    Apply changes from main repo

--- src/lib/utils/api/fetchSendLog.ts
+++ src/lib/utils/api/fetchSendLog.ts
@@ -1,10 +1,12 @@
+import { stringify } from "circular-json"
+
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const fetchSendLog = async (params: any) => {
   try {
     fetch(`${BASE_URL}/log`, {
       method: 'POST',
-      body: JSON.stringify(params),
+      body: stringify(params),
       credentials: 'include',
     });
   } catch (error) {

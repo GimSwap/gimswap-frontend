3ee238ec2090e92dc1a9cbbf182280581cc9faea
GimSwap <admin@gimswap.com>
2024-12-03 09:16:09 +0000

    Apply changes from main repo

--- src/lib/hook/useSwap.tsx
+++ src/lib/hook/useSwap.tsx
@@ -1,14 +1,16 @@
-import swapAbi from "@/src/lib/utils/abis/swapAbi.json";
-import { TokenType } from "../types/TokenType";
-import { makeSwapArgument } from "../utils/makeSwapArgument";
-import { safeCalc } from "../utils/safeCalc";
-import { CONTRACT_ADDRESS } from "../constants/contractAddress";
-import { useState } from "react";
-import { createPublicClient, createWalletClient, custom } from "viem";
-import { kaia, kairos } from "wagmi/chains";
-import { http, useAccount } from "wagmi";
-import { WALLETS } from "@/src/lib/constants/wallets";
-import { fetchSendLog } from "../utils/api/fetchSendLog";
+import swapAbi from '@/src/lib/utils/abis/swapAbi.json';
+import { TokenType } from '../types/TokenType';
+import { makeSwapArgument } from '../utils/makeSwapArgument';
+import { safeCalc } from '../utils/safeCalc';
+import { CONTRACT_ADDRESS } from '../constants/contractAddress';
+import { useState } from 'react';
+import { createWalletClient, custom } from 'viem';
+import { kaia, kairos } from 'wagmi/chains';
+import { useAccount } from 'wagmi';
+import { WALLETS } from '@/src/lib/constants/wallets';
+import { fetchSendLog } from '../utils/api/fetchSendLog';
+import { waitForTransactionReceipt } from '@wagmi/core';
+import { wagmiConfig } from '../utils/wagmi';
 
 export const useSwap = (token: TokenType, amount: string) => {
   const [isPending, setIsPending] = useState<boolean>(false);
@@ -28,15 +30,13 @@ export const useSwap = (token: TokenType, amount: string) => {
   const callee = CONTRACT_ADDRESS.GimSwap;
 
   const network =
-    process.env.NEXT_PUBLIC_ENV_MODE !== "production" ? kairos : kaia;
-  const publicClient = createPublicClient({
-    chain: network,
-    transport: http(),
-  });
+    process.env.NEXT_PUBLIC_ENV_MODE !== 'production' ? kairos : kaia;
 
   const swap = async () => {
     if (!connector || !address) return;
-    const currentWalletInfo = WALLETS.find(({ id }) => connector.id.replace(/\s+/g, '').toLowerCase().includes(id.toLowerCase()));
+    const currentWalletInfo = WALLETS.find(({ id }) =>
+      connector.id.replace(/\s+/g, '').toLowerCase().includes(id.toLowerCase()),
+    );
 
     try {
       const walletClient = createWalletClient({
@@ -58,15 +58,20 @@ export const useSwap = (token: TokenType, amount: string) => {
         account: address as `0x${string}`,
         args,
       });
-
       if (!hash) throw new Error(`transaction error`);
       setHash(hash);
-      const { status } = await publicClient.waitForTransactionReceipt({ hash });
-      if (status === "success") setIsSuccess(true);
-      else if (status === "reverted") setIsError(true);
+      const { status } = await waitForTransactionReceipt(wagmiConfig, { hash });
+      if (status === 'success') setIsSuccess(true);
+      else if (status === 'reverted') setIsError(true);
     } catch (error) {
-      const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-      fetchSendLog({ name: 'swap', error: errorMessage, currentWalletInfoId: currentWalletInfo?.id, connectorId: connector.id });
+      const errorMessage =
+        error instanceof Error ? error.message : 'Unknown error';
+      fetchSendLog({
+        name: 'swap',
+        error: errorMessage,
+        currentWalletInfoId: currentWalletInfo?.id,
+        connectorId: connector.id,
+      });
       setIsError(true);
     } finally {
       setIsPending(false);
